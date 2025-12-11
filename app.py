c0d452cda4ec5d6a2bee6c43f2ea23a714e65684
ynaivasor <124240128@student.upnyk.ac.id>
2025-12-11 19:07:37 +0700

    udah bisa jalan tapi agak cacat

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request, redirect
+from fuzzy_spk import hitung_fuzzy
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def kriteria():
+    return render_template("kriteria.html")
+
+@app.route('/process', methods=['POST'])
+def process():
+    harga = int(request.form['harga'])
+    jarak = int(request.form['jarak'])
+    fasilitas = int(request.form['fasilitas'])
+
+    hasil = hitung_fuzzy(harga, jarak, fasilitas)  
+
+    if hasil == "santai":
+        return redirect('/hasil_santai')
+    elif hasil == "mato":
+        return redirect('/hasil_mato')
+    elif hasil == "bento":
+        return redirect('/hasil_bento')
+    elif hasil == "senja":
+        return redirect('/hasil_senja')
+    else:
+        return redirect('/hasil_mato')  # fallback
+
+@app.route('/hasil_santai')
+def hasil_santai():
+    return render_template("hasilSantai.html")
+
+@app.route('/hasil_mato')
+def hasil_mato():
+    return render_template("hasilMato.html")
+
+@app.route('/hasil_bento')
+def hasil_bento():
+    return render_template("hasilBento.html")
+
+@app.route('/hasil_senja')
+def hasil_senja():
+    return render_template("hasilSenja.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- fuzzy_spk.py
+++ fuzzy_spk.py
@@ -2,16 +2,11 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
-# ====================================================
-# DEFINISI
-# ====================================================
-
 harga = ctrl.Antecedent(np.arange(0, 101, 1), 'Harga')
 jarak = ctrl.Antecedent(np.arange(0, 101, 1), 'Jarak')
 fasilitas = ctrl.Antecedent(np.arange(0, 101, 1), 'Fasilitas')
 rekomendasi = ctrl.Consequent(np.arange(0, 101, 1), 'Rekomendasi')
 
-# Membership Function
 harga['murah'] = fuzz.trapmf(harga.universe, [0, 0, 20, 40])
 harga['normal'] = fuzz.trimf(harga.universe, [30, 50, 70])
 harga['mahal'] = fuzz.trapmf(harga.universe, [60, 80, 100, 100])
@@ -24,21 +19,68 @@ fasilitas['tidak lengkap'] = fuzz.trapmf(fasilitas.universe, [0, 0, 20, 40])
 fasilitas['lengkap'] = fuzz.trimf(fasilitas.universe, [30, 50, 70])
 fasilitas['sangat lengkap'] = fuzz.trapmf(fasilitas.universe, [60, 80, 100, 100])
 
-rekomendasi['TD'] = fuzz.trapmf(rekomendasi.universe, [0, 0, 20, 40])
-rekomendasi['C'] = fuzz.trimf(rekomendasi.universe, [30, 50, 70])
-rekomendasi['D'] = fuzz.trimf(rekomendasi.universe, [60, 75, 90])
-rekomendasi['SD'] = fuzz.trapmf(rekomendasi.universe, [80, 90, 100, 100])
+rekomendasi['TD'] = fuzz.trapmf(rekomendasi.universe, [0, 0, 20, 40])     # Senja
+rekomendasi['C']  = fuzz.trimf(rekomendasi.universe, [30, 50, 70])        # Bento
+rekomendasi['D']  = fuzz.trimf(rekomendasi.universe, [60, 75, 90])        # MaTo
+rekomendasi['SD'] = fuzz.trapmf(rekomendasi.universe, [80, 90, 100, 100]) # Santai Kawan
 
-# Rules
-rules = []
-# (rules kamu tetap, tidak perlu diubah)
+rules = [
+
+    ctrl.Rule(harga['murah'] & jarak['dekat'] & fasilitas['sangat lengkap'], rekomendasi['SD']),
+    ctrl.Rule(harga['murah'] & jarak['dekat'] & fasilitas['lengkap'], rekomendasi['SD']),
+    ctrl.Rule(harga['murah'] & jarak['dekat'] & fasilitas['tidak lengkap'], rekomendasi['D']),
+
+    ctrl.Rule(harga['murah'] & jarak['sedang'] & fasilitas['sangat lengkap'], rekomendasi['D']),
+    ctrl.Rule(harga['murah'] & jarak['sedang'] & fasilitas['lengkap'], rekomendasi['D']),
+    ctrl.Rule(harga['murah'] & jarak['sedang'] & fasilitas['tidak lengkap'], rekomendasi['C']),
+
+    ctrl.Rule(harga['murah'] & jarak['jauh'] & fasilitas['sangat lengkap'], rekomendasi['C']),
+    ctrl.Rule(harga['murah'] & jarak['jauh'] & fasilitas['lengkap'], rekomendasi['C']),
+    ctrl.Rule(harga['murah'] & jarak['jauh'] & fasilitas['tidak lengkap'], rekomendasi['TD']),
+
+    ctrl.Rule(harga['normal'] & jarak['dekat'] & fasilitas['sangat lengkap'], rekomendasi['D']),
+    ctrl.Rule(harga['normal'] & jarak['dekat'] & fasilitas['lengkap'], rekomendasi['D']),
+    ctrl.Rule(harga['normal'] & jarak['dekat'] & fasilitas['tidak lengkap'], rekomendasi['C']),
+
+    ctrl.Rule(harga['normal'] & jarak['sedang'] & fasilitas['sangat lengkap'], rekomendasi['C']),
+    ctrl.Rule(harga['normal'] & jarak['sedang'] & fasilitas['lengkap'], rekomendasi['C']),
+    ctrl.Rule(harga['normal'] & jarak['sedang'] & fasilitas['tidak lengkap'], rekomendasi['TD']),
+
+    ctrl.Rule(harga['normal'] & jarak['jauh'] & fasilitas['sangat lengkap'], rekomendasi['TD']),
+    ctrl.Rule(harga['normal'] & jarak['jauh'] & fasilitas['lengkap'], rekomendasi['TD']),
+    ctrl.Rule(harga['normal'] & jarak['jauh'] & fasilitas['tidak lengkap'], rekomendasi['TD']),
+
+    ctrl.Rule(harga['mahal'] & jarak['dekat'] & fasilitas['sangat lengkap'], rekomendasi['C']),
+    ctrl.Rule(harga['mahal'] & jarak['dekat'] & fasilitas['lengkap'], rekomendasi['C']),
+    ctrl.Rule(harga['mahal'] & jarak['dekat'] & fasilitas['tidak lengkap'], rekomendasi['TD']),
+
+    ctrl.Rule(harga['mahal'] & jarak['sedang'] & fasilitas['sangat lengkap'], rekomendasi['TD']),
+    ctrl.Rule(harga['mahal'] & jarak['sedang'] & fasilitas['lengkap'], rekomendasi['TD']),
+    ctrl.Rule(harga['mahal'] & jarak['sedang'] & fasilitas['tidak lengkap'], rekomendasi['TD']),
+
+    ctrl.Rule(harga['mahal'] & jarak['jauh'] & fasilitas['sangat lengkap'], rekomendasi['TD']),
+    ctrl.Rule(harga['mahal'] & jarak['jauh'] & fasilitas['lengkap'], rekomendasi['TD']),
+    ctrl.Rule(harga['mahal'] & jarak['jauh'] & fasilitas['tidak lengkap'], rekomendasi['TD']),
+
+]
+
 
 rekom_ctrl = ctrl.ControlSystem(rules)
 rekom_sim = ctrl.ControlSystemSimulation(rekom_ctrl)
 
-def hitung_rekomendasi(harga_input, jarak_input, fasilitas_input):
+def hitung_fuzzy(harga_input, jarak_input, fasilitas_input):
     rekom_sim.input['Harga'] = harga_input
     rekom_sim.input['Jarak'] = jarak_input
     rekom_sim.input['Fasilitas'] = fasilitas_input
+
     rekom_sim.compute()
-    return rekom_sim.output['Rekomendasi']
+    nilai = rekom_sim.output['Rekomendasi']
+
+    if nilai >= 80:
+        return "santai"   
+    elif nilai >= 60:
+        return "mato"     
+    elif nilai >= 40:
+        return "bento"    
+    else:
+        return "senja"    
